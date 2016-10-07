de710e3ad9af5218b8463217268d9e9311ac7b09
Montel Laurent <montel@kde.org>
2016-10-07 08:21:06 +0200

    Continue to create test apps

--- src/listview/tests/addressbookmainwidget.cpp
+++ src/listview/tests/addressbookmainwidget.cpp
@@ -24,7 +24,139 @@ AddressBookMainWidget::AddressBookMainWidget(QWidget *parent)
     : QWidget(parent)
 {
     QVBoxLayout *mainLayout = new QVBoxLayout(this);
+#if 0
+    mCollectionTree = new Akonadi::EntityMimeTypeFilterModel(this);
+    mCollectionTree->setDynamicSortFilter(true);
+    mCollectionTree->setSortCaseSensitivity(Qt::CaseInsensitive);
+    mCollectionTree->setSourceModel(GlobalContactModel::instance()->model());
+    mCollectionTree->addMimeTypeInclusionFilter(Akonadi::Collection::mimeType());
+    mCollectionTree->setHeaderGroup(Akonadi::EntityTreeModel::CollectionTreeHeaders);
 
+    mCollectionSelectionModel = new QItemSelectionModel(mCollectionTree);
+    StructuralCollectionsNotCheckableProxy *checkableProxyModel =
+        new StructuralCollectionsNotCheckableProxy(this);
+    checkableProxyModel->setSelectionModel(mCollectionSelectionModel);
+    checkableProxyModel->setSourceModel(mCollectionTree);
+
+    mCollectionView->setModel(checkableProxyModel);
+    mCollectionView->setXmlGuiClient(guiClient);
+    mCollectionView->header()->setDefaultAlignment(Qt::AlignCenter);
+    mCollectionView->header()->setSortIndicatorShown(false);
+
+    connect(mCollectionView->model(), &QAbstractItemModel::rowsInserted,
+            this, &MainWidget::slotCheckNewCalendar);
+
+    connect(mCollectionView, SIGNAL(currentChanged(Akonadi::Collection)),
+            this, SLOT(slotCurrentCollectionChanged(Akonadi::Collection)));
+
+    KSelectionProxyModel *selectionProxyModel =
+        new KSelectionProxyModel(mCollectionSelectionModel, this);
+    selectionProxyModel->setSourceModel(GlobalContactModel::instance()->model());
+    selectionProxyModel->setFilterBehavior(KSelectionProxyModel::ChildrenOfExactSelection);
+
+    mItemTree = new Akonadi::EntityMimeTypeFilterModel(this);
+    mItemTree->setSourceModel(selectionProxyModel);
+    mItemTree->addMimeTypeExclusionFilter(Akonadi::Collection::mimeType());
+    mItemTree->setHeaderGroup(Akonadi::EntityTreeModel::ItemListHeaders);
+
+    mCategoryFilterModel = new CategoryFilterProxyModel(this);
+    mCategoryFilterModel->setSourceModel(mItemTree);
+    mCategoryFilterModel->setFilterCategories(mCategorySelectWidget->filterTags());
+    mCategoryFilterModel->setFilterEnabled(true);
+
+    connect(mCategorySelectWidget, &CategorySelectWidget::filterChanged,
+            mCategoryFilterModel, &CategoryFilterProxyModel::setFilterCategories);
+
+    mContactsFilterModel = new Akonadi::ContactsFilterProxyModel(this);
+    mContactsFilterModel->setSourceModel(mCategoryFilterModel);
+
+    connect(mQuickSearchWidget, &QuickSearchWidget::filterStringChanged,
+            mContactsFilterModel, &Akonadi::ContactsFilterProxyModel::setFilterString);
+    connect(mQuickSearchWidget, &QuickSearchWidget::filterStringChanged,
+            this, &MainWidget::selectFirstItem);
+    connect(mQuickSearchWidget, &QuickSearchWidget::arrowDownKeyPressed,
+            this, &MainWidget::setFocusToTreeView);
+    mItemView->setModel(mContactsFilterModel);
+    mItemView->setXmlGuiClient(guiClient);
+    mItemView->setSelectionMode(QAbstractItemView::ExtendedSelection);
+    mItemView->setRootIsDecorated(false);
+    mItemView->header()->setDefaultAlignment(Qt::AlignCenter);
+
+    mXXPortManager->setSelectionModel(mItemView->selectionModel());
+
+    mActionManager = new Akonadi::StandardContactActionManager(guiClient->actionCollection(), this);
+    mActionManager->setCollectionSelectionModel(mCollectionView->selectionModel());
+    mActionManager->setItemSelectionModel(mItemView->selectionModel());
+
+    QList<Akonadi::StandardActionManager::Type> standardActions;
+    standardActions << Akonadi::StandardActionManager::CreateCollection
+                    << Akonadi::StandardActionManager::DeleteCollections
+                    << Akonadi::StandardActionManager::SynchronizeCollections
+                    << Akonadi::StandardActionManager::CollectionProperties
+                    << Akonadi::StandardActionManager::CopyItems
+                    << Akonadi::StandardActionManager::Paste
+                    << Akonadi::StandardActionManager::DeleteItems
+                    << Akonadi::StandardActionManager::CutItems
+                    << Akonadi::StandardActionManager::CreateResource
+                    << Akonadi::StandardActionManager::DeleteResources
+                    << Akonadi::StandardActionManager::ResourceProperties
+                    << Akonadi::StandardActionManager::SynchronizeResources
+                    << Akonadi::StandardActionManager::SynchronizeCollectionsRecursive
+                    << Akonadi::StandardActionManager::MoveItemToMenu
+                    << Akonadi::StandardActionManager::CopyItemToMenu;
+
+    Q_FOREACH (Akonadi::StandardActionManager::Type standardAction, standardActions) {
+        mActionManager->createAction(standardAction);
+    }
+    guiClient->actionCollection()->setDefaultShortcut(mActionManager->action(Akonadi::StandardActionManager::DeleteItems), QKeySequence(Qt::Key_Delete));
+    QList<Akonadi::StandardContactActionManager::Type> contactActions;
+    contactActions << Akonadi::StandardContactActionManager::CreateContact
+                   << Akonadi::StandardContactActionManager::CreateContactGroup
+                   << Akonadi::StandardContactActionManager::EditItem;
+
+    Q_FOREACH (Akonadi::StandardContactActionManager::Type contactAction, contactActions) {
+        mActionManager->createAction(contactAction);
+    }
+    static bool pageRegistered = false;
+
+    if (!pageRegistered) {
+        Akonadi::CollectionPropertiesDialog::registerPage(new PimCommon::CollectionAclPageFactory);
+        pageRegistered = true;
+    }
+
+    const QStringList pages =
+        QStringList() << QStringLiteral("Akonadi::CollectionGeneralPropertiesPage")
+        << QStringLiteral("Akonadi::CachePolicyPage")
+        << QStringLiteral("PimCommon::CollectionAclPage");
+
+    mActionManager->setCollectionPropertiesPageNames(pages);
+
+    connect(mItemView, SIGNAL(currentChanged(Akonadi::Item)),
+            this, SLOT(itemSelected(Akonadi::Item)));
+    connect(mItemView, SIGNAL(doubleClicked(Akonadi::Item)),
+            mActionManager->action(Akonadi::StandardContactActionManager::EditItem),
+            SLOT(trigger()));
+    connect(mItemView->selectionModel(), &QItemSelectionModel::currentChanged,
+            this, &MainWidget::itemSelectionChanged);
+    connect(mItemView->selectionModel(), &QItemSelectionModel::selectionChanged,
+            this, &MainWidget::slotSelectionChanged);
+
+    // show the contact details view as default
+    mDetailsViewStack->setCurrentWidget(mContactDetails);
+
+    mContactSwitcher->setView(mItemView);
+
+    Akonadi::ControlGui::widgetNeedsAkonadi(this);
+
+    mModelColumnManager = new ModelColumnManager(GlobalContactModel::instance()->model(), this);
+    mModelColumnManager->setWidget(mItemView->header());
+    mModelColumnManager->load();
+
+    QMetaObject::invokeMethod(this, "delayedInit", Qt::QueuedConnection);
+    updateQuickSearchText();
+    slotSelectionChanged();
+
+#endif
 }
 
 AddressBookMainWidget::~AddressBookMainWidget()
