1d5a601bf13447e31cf144e9ae59c38dc94de66e
Montel Laurent <montel@kde.org>
2017-01-05 20:57:28 +0100

    GIT_SILENT: fix copyright

--- src/domain/contact.h
+++ src/domain/contact.h
@@ -1,7 +1,7 @@
 /*
   This file is part of KAddressBook.
 
-  Copyright (c) 2016 Laurent Montel <montel@kde.org>
+  Copyright (C) 2016-2017 Laurent Montel <montel@kde.org>
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
