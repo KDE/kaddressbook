46fa6acba6f207ee2cd2db949e033e761799614e
Pino Toscano <pino@kde.org>
2019-07-05 10:44:26 +0200

    i18n: use rc.cpp for extra sources
    Use rc.cpp as only dummy source for non-C strings, so repack-pot is
    happier.

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 $EXTRACTRC `find . -name \*.ui` `find . -name \*.rc` >> rc.cpp || exit 11
-$EXTRACT_GRANTLEE_TEMPLATE_STRINGS `find . -name \*.html` >> html.cpp
+$EXTRACT_GRANTLEE_TEMPLATE_STRINGS `find . -name \*.html` >> rc.cpp
 $XGETTEXT `find -name \*.cpp -o -name \*.h` -o $podir/kaddressbook.pot
-rm -f rc.cpp html.cpp
+rm -f rc.cpp
