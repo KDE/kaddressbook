2e7337718bb6eb6d101737c07a5a9a91f987b519
Laurent Montel <montel@kde.org>
2019-09-02 10:41:38 +0200

    GIT_SILENT: minor qstring optimization

--- src/printing/printstyle.cpp
+++ src/printing/printstyle.cpp
@@ -59,7 +59,7 @@ bool PrintStyle::setPreview(const QString &fileName)
 {
     QPixmap preview;
 
-    const QString path = QStandardPaths::locate(QStandardPaths::GenericDataLocation, QStringLiteral("kaddressbook/printing/") + fileName);
+    const QString path = QStandardPaths::locate(QStandardPaths::GenericDataLocation, QLatin1String("kaddressbook/printing/") + fileName);
     if (path.isEmpty()) {
         qCDebug(KADDRESSBOOK_LOG) << "cannot locate preview image " << fileName << " in appdata";
         return false;
