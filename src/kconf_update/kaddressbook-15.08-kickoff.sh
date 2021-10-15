708099073de2c8eb5f656a3760c7f610ce404ee9
Laurent Montel <montel@kde.org>
2021-10-15 06:49:45 +0200

    GIT_SILENT: remove old kconf_update

--- src/kconf_update/kaddressbook-15.08-kickoff.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-kickoffrcname=`qtpaths --locate-file  GenericConfigLocation kickoffrc`
-if [ -f "$kickoffrcname" ]; then
-   sed -i "s/\/kaddressbook.desktop/\/org.kde.kaddressbook.desktop/" $kickoffrcname
-fi
