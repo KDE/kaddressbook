4a7496f7fd27d5106db8cd25480171195f312ce9
Laurent Montel <montel@kde.org>
2022-02-22 13:27:41 +0100

    Use QListIterator in qt6

--- src/importexport/pluginmanager.cpp
+++ src/importexport/pluginmanager.cpp
@@ -78,7 +78,11 @@ bool PluginManagerPrivate::initializePlugins()
 
     const QPair<QStringList, QStringList> pair = PimCommon::PluginUtil::loadPluginSetting(configGroupName(), configPrefixSettingKey());
 
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QVectorIterator<KPluginMetaData> i(plugins);
+#else
+    QListIterator<KPluginMetaData> i(plugins);
+#endif
     i.toBack();
     while (i.hasPrevious()) {
         ImportExportInfo info;
