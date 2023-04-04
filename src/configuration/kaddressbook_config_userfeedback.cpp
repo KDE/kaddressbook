dba7feca125300ab191f5cf0f35610a621c2d5de
Laurent Montel <montel@kde.org>
2023-04-04 20:08:00 +0200

    Use KUserFeedBackQt6

--- src/configuration/kaddressbook_config_userfeedback.cpp
+++ src/configuration/kaddressbook_config_userfeedback.cpp
@@ -11,11 +11,7 @@
 #include <QHBoxLayout>
 #ifdef WITH_KUSERFEEDBACK
 #include "userfeedback/userfeedbackmanager.h"
-#ifdef USE_KUSERFEEDBACK_QT6
 #include <KUserFeedbackQt6/FeedbackConfigWidget>
-#else
-#include <KUserFeedback/FeedbackConfigWidget>
-#endif
 #endif
 
 using namespace KAddressBook;
--- src/userfeedback/kaddressbookuserfeedbackprovider.h
+++ src/userfeedback/kaddressbookuserfeedbackprovider.h
@@ -7,11 +7,7 @@
 #pragma once
 
 #include "kaddressbook_export.h"
-#ifdef USE_KUSERFEEDBACK_QT6
 #include <KUserFeedbackQt6/Provider>
-#else
-#include <KUserFeedback/Provider>
-#endif
 class KADDRESSBOOK_EXPORT KAddressBookUserFeedbackProvider : public KUserFeedback::Provider
 {
     Q_OBJECT
