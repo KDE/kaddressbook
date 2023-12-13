e46ae90b72964b6c4c4bdaa82f9d4dcceb925e75
Laurent Montel <montel@kde.org>
2023-12-13 14:00:18 +0100

    Remove spacing

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -42,6 +42,7 @@ MainWindow::MainWindow()
     auto mainWidget = new QWidget(this);
     auto mainWidgetLayout = new QVBoxLayout(mainWidget);
     mainWidgetLayout->setContentsMargins({});
+    mainWidgetLayout->setSpacing(0);
     if (PimCommon::NeedUpdateVersionUtils::checkVersion()) {
         const auto status = PimCommon::NeedUpdateVersionUtils::obsoleteVersionStatus(KAboutData::applicationData().version(), QDate::currentDate());
         if (status != PimCommon::NeedUpdateVersionUtils::ObsoleteVersion::NotObsoleteYet) {
