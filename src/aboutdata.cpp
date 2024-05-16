0b6656e93426bb9ef3a3982019cdd617e8ba0073
Laurent Montel <montel@kde.org>
2024-05-16 07:19:14 +0200

    Use credit

--- src/aboutdata.cpp
+++ src/aboutdata.cpp
@@ -20,10 +20,13 @@ AboutData::AboutData()
                  KAboutLicense::GPL_V2,
                  i18n("Copyright © 2007–%1 KAddressBook authors", QStringLiteral("2024")))
 {
-    addAuthor(i18n("Laurent Montel"), i18n("Current maintainer"), QStringLiteral("montel@kde.org"));
-    addAuthor(i18n("Tobias König"), i18n("Previous maintainer"), QStringLiteral("tokoe@kde.org"));
-    addAuthor(i18n("Stefan Taferner"), i18n("Original Author (1998)"), QStringLiteral("taferner@kde.org"));
-    addAuthor(i18n("David Bryant"), i18n("Documentation (2019)"), QStringLiteral("davidbryant@gvtc.com"), QStringLiteral("https://davidcbryant.net"));
+    addAuthor(i18nc("@info:credit", "Laurent Montel"), i18n("Current maintainer"), QStringLiteral("montel@kde.org"));
+    addAuthor(i18nc("@info:credit", "Tobias König"), i18n("Previous maintainer"), QStringLiteral("tokoe@kde.org"));
+    addAuthor(i18nc("@info:credit", "Stefan Taferner"), i18n("Original Author (1998)"), QStringLiteral("taferner@kde.org"));
+    addAuthor(i18nc("@info:credit", "David Bryant"),
+              i18n("Documentation (2019)"),
+              QStringLiteral("davidbryant@gvtc.com"),
+              QStringLiteral("https://davidcbryant.net"));
 }
 
 AboutData::~AboutData() = default;
