028b5e1a2adfd03bea8cd8d7d569842602c4cdec
Laurent Montel <montel@kde.org>
2024-10-10 07:36:37 +0200

    Add TODO for setEnablePlasmaActivities

--- src/mainwidget.cpp
+++ src/mainwidget.cpp
@@ -210,6 +210,7 @@ MainWidget::MainWidget(KXMLGUIClient *guiClient, QWidget *parent)
     mCollectionTree = new ContactEntityMimeTypeFilterModel(this);
 #if HAVE_ACTIVITY_SUPPORT
     mCollectionTree->setAccountActivities(ActivitiesManager::self()->accountActivities());
+    // TODO mCollectionTree->setEnablePlasmaActivities();
 #endif
     mCollectionTree->setSortCaseSensitivity(Qt::CaseInsensitive);
     mCollectionTree->setSourceModel(GlobalContactModel::instance()->model());
