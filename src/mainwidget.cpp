95aec9dae58aec175c86e3f1f8394c6df935c650
Balló György <ballogyor@gmail.com>
2025-03-19 19:29:37 +0000

    Fix color scheme change
    it needs to monitor the PaletteChange event instead of ApplicationPaletteChange to work.

--- src/mainwidget.cpp
+++ src/mainwidget.cpp
@@ -342,7 +342,7 @@ void MainWidget::slotGeneralPaletteChanged()
 
 bool MainWidget::event(QEvent *e)
 {
-    if (e->type() == QEvent::ApplicationPaletteChange) {
+    if (e->type() == QEvent::PaletteChange) {
         slotGeneralPaletteChanged();
     }
     return QWidget::event(e);
